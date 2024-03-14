621d04e179dc270881eb217dead5f212548a8434
nanobloom <nanobloom@github.com>
2024-03-14 21:16:47 +0100

    Added inner/around function/class treesitter keybinds

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,15 +5,42 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				ensure_installed = { "javascript", "typescript", "python", "c", "lua", "rust", "vim", "vimdoc", "query" },
+				ensure_installed = {
+					"javascript",
+					"typescript",
+					"python",
+					"c",
+					"lua",
+					"rust",
+					"vim",
+					"vimdoc",
+					"query",
+				},
 				sync_install = false,
 				auto_install = true,
 				highlight = {
 					enable = true,
 					additional_vim_regex_highlighting = false,
 				},
-	--			indent = { enable = true },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
+				--			indent = { enable = true },
 			})
-		end
-	}
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		init = function()
+		end,
+	},
 }

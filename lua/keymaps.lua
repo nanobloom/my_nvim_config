9cfac1b06109029aa3216255042efe67ab8ebcc8
nanobloom <nanobloom@github.com>
2024-03-14 21:15:49 +0100

    Added diagnostics keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,7 +33,11 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Define autocommands for automatic search highlighting toggling
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })-- Define autocommands for automatic search highlighting toggling
+
 vim.cmd([[
   augroup AutoHighlighting
     autocmd!

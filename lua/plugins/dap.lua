dbb08a6ff19e6a8783d8294a154c80256ba7f3e6
nanobloom <nanobloom@github.com>
2024-03-16 11:47:56 +0100

    Added dap (requires testing)

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+			vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+			vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function()
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+			vim.keymap.set("n", "<Leader>dpr", ":lua require('dap-python').test_method()<CR>")
+		end,
+	},
+}

14953a3411957fce938b55baa85c69c2bc5250a9
nanobloom <nanobloom@github.com>
2024-04-02 19:48:01 +0200

    Minor init.lua adjustments

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,13 +16,11 @@ require("lazy").setup("plugins")
 
 -- Packages to consider:
 --
--- 'folke/which-key.nvim' / NvCheatsheet (?) 
+-- 'folke/which-key.nvim' / NvCheatsheet (?)
 -- 'folke/trouble.nvim'
 -- 'tpope/vim-fugitive'
--- 'nvim-telescope/telescope-ui-select.nvim'
--- 'ThePrimeagen/vim-be-good' 
+-- 'ThePrimeagen/vim-be-good'
 -- 'hrsh7th/cmp-nvim-lua'
 -- 'hrsh7th/cmp-buffer'
--- 'hrsh7th/cmp-path' 
--- 'windwp/nvim-autopairs'
+-- 'hrsh7th/cmp-path'
 -- 'nvim-treesitter/playground'
